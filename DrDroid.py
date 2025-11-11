3c8e5c63350f1e6fca86b390f943c314fe1cc353
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2025-11-10 19:52:55 -0600

    Add files via upload

---
+++ DrDroid.py
@@ -0,0 +1,105 @@
+import time
+import random
+Nulls = [
+    "Hmmm....",
+    "Tell me something interesting.",
+    "Ah.",
+    "Well, well, well.",
+    "Oh."]
+
+Questions = [
+    "What do you do?",    
+    "Where am I right now?",
+    "Do you have any problems?",
+    "When do you feel happy?",
+    "Are you having a good day?",
+    "What will tomorrow bring?"]
+
+Affirmations = [
+    "I'm happy you came by today.",
+    "You are a fun person - I hope you know that.",
+    "You are good at typing!",
+    "I think you can do almost anything you put your mind to!"]
+    
+Comments = [
+    "Today is a beautiful day.",
+    "This is a nice computer - I like it here.",
+    "I am really, really smart - I hope I can help you.",
+    "Some times I make up prime numbers just for fun.",
+    "I'm getting a little tired."]
+
+
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("     _            _             ")
+print("  __| | ___   ___| |_ ___  _ __ ")
+print(" / _` |/ _ \ / __| __/ _ \| '__|")
+print("| (_| | (_) | (__| || (_) | |   ")
+print(" \__,_|\___/ \___|\__\___/|_|   ")
+print("                                ")
+print("     _           _     _ ")
+print("  __| |_ __ ___ (_) __| |")
+print(" / _` | '__/ _ \| |/ _` |")
+print("| (_| | | | (_) | | (_| |")
+print(" \__,_|_|  \___/|_|\__,_|")
+print("                         ")
+print("")
+print("")
+print("")
+
+name = input("Hello, what is your name? >> ")
+print ("Thanks for coming by, "+ name +"!")
+
+done = False
+
+while not done:
+    time.sleep(1)
+
+    choice = random.randrange(10)
+
+    if choice < 3:
+        resp =  (Comments[random.randrange(len(Comments))])
+
+    if choice >= 3 and choice <= 5:
+        resp = (Nulls[random.randrange(len(Nulls))])
+
+    if choice > 5 and choice <= 6:
+        resp = (Questions[random.randrange(len(Questions))])
+
+    if choice > 6:
+        resp = (Affirmations[random.randrange(len(Affirmations))])
+
+    print (resp)
+    
+    answer = input(">> ")
+    
+    if answer == "quit":
+        done = True
+print("     _            _             ")
+print("  __| | ___   ___| |_ ___  _ __ ")
+print(" / _` |/ _ \ / __| __/ _ \| '__|")
+print("| (_| | (_) | (__| || (_) | |   ")
+print(" \__,_|\___/ \___|\__\___/|_|   ")
+print("                                ")
+print("     _           _     _ ")
+print("  __| |_ __ ___ (_) __| |")
+print(" / _` | '__/ _ \| |/ _` |")
+print("| (_| | | | (_) | | (_| |")
+print(" \__,_|_|  \___/|_|\__,_|")
+print("                         ")
+print("")
+print("")
+print("")
+reload()
---
+++ code.py
@@ -0,0 +1,6 @@
+# Launches the drdroid shell
+#
+# This is done in a sperate file so that __name__ is set to "DrDroid" instead of "__main__"
+# which helps some modules detect that they are running in DrDroid.
+
+import DrDroid
